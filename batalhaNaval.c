d679f1a585e52d06848fcf11c6f7b39404c28ca2
Felipe Lima <felipelimabox@gmail.com>
2025-05-14 19:26:23 +0000

    Adicionando mais 2 navis. Implementação da lógica anterior. Nível aventureiro funcionando.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,49 +5,49 @@
 
 int main() {
 
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    /* Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
+        Sugestão: Expanda o tabuleiro para uma matriz 10x10.
+        Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
+        Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.*/
 
-    //Declarando vetores para linhas (1-10) e colunas (A-J):
-
-    char coluna[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; 
-    int linha[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-    //Declarando matriz para tabuleiro de 10x10:
-
-    int matriztabuleiro [10][10] = {0}; 
-
-    //Iniciando o jogo:
-
-    printf("\n---- Seja bem-vindo ao jogo Batalha Naval ----\n");
-
-    // Posicionando os navios (Horizontal e Vertical) representados por 3.
-    //Horizontal:
-     matriztabuleiro[2][2] = 3; 
-     matriztabuleiro[2][3] = 3;
-     matriztabuleiro[2][4] = 3;
- 
-    //Vertical:
-     matriztabuleiro[6][7] = 3; // vertical
-     matriztabuleiro[7][7] = 3;
-     matriztabuleiro[8][7] = 3;
+    //Declarando inicialização de Matriz para representar tabuleiro.
+    //Valores em zero representa água.
     
-    
-    // for para o cabeçalho das colunas da matriz
-    printf("  ");
-    for (int j = 0; j < 10; j++) {
-        printf(" %c", coluna[j]);
+    int tabuleiro[10][10] = {0}; 
+
+    // Declarando valores em 3 para representar navio posição: Horizontal
+    for (int navio1 = 0; navio1 < 0 + 3; navio1++){
+    tabuleiro[4][navio1] = 3; 
     }
-    printf("\n");
 
+    // Declarando valores em 3 para representar navio posição: Vertical
+    for (int navio2 = 7; navio2 < 7 + 3; navio2++){
+        tabuleiro[navio2][9] = 3; 
+    }
 
-    // for para as linhas da matriz
-    for (int i = 0; i < 10; i++) {
-        printf("%2d", linha[i]); 
-        for (int j = 0; j < 10; j++) {
-            printf(" %d", matriztabuleiro[i][j]);
+    // Declarando valores em 3 para representar navio posição: Diagonal direita
+    for (int navio3 = 0; navio3 < 3; navio3++) {
+    tabuleiro[0 + navio3][0 + navio3] = 3;
+    }
+
+    // Declarando valores em 3 para representar navio posição: Diagonal esquerda
+    for (int navio4 = 0; navio4 < 3; navio4++) {
+    tabuleiro[7 + navio4][2 - navio4] = 3;
+    }
+
+    // Imprimindo o cabeçalho com letras de A a J (coluna)
+    // Espaço para índice
+    printf("   "); 
+        for (char coluna = 'A'; coluna <= 'J'; coluna++) {
+            printf(" %c", coluna);
+        }
+        printf("\n");
+
+    // Declarando estrutura for para imprimir linhas numéricas de 1 a 10
+    for (int a = 0; a < 10; a++) {
+        printf("%2d ", a + 1); 
+        for (int b = 0; b < 10; b++) {
+            printf(" %d", tabuleiro[a][b]);
         }
         printf("\n");
     }
